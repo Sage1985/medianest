2458c5a898c0d9b7d663b92288b872140c46c909
Emmanuel Valencia Vazquez <144173029+Sage1985@users.noreply.github.com>
2026-07-18 23:26:12 -0600

    Publicar MediaNest TV

---
+++ app/scripts/config.js
@@ -0,0 +1,6 @@
+window.MediaNestConfig = {
+  jamendoClientId: "",
+  // Configura una clave propia en una copia local; no se publica en el repositorio.
+  youtubeApiKey: "",
+  youtubePlayerUrl: "https://sage1985.github.io/HomeWorks/"
+};
